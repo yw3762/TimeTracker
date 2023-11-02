ad12bc34065712e87bc03e41314c1dd437434cd7
Yifan Wang <8groupoid@gmail.com>
2023-11-02 15:05:39 -0400

    Initial Commit

---
+++ TimeTracking/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TimeTracking
+//
+//  Created by Alex Wang on 11/2/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TimeTracking/TimeTrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeTrackingApp.swift
+//  TimeTracking
+//
+//  Created by Alex Wang on 11/2/23.
+//
+
+import SwiftUI
+
+@main
+struct TimeTrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
